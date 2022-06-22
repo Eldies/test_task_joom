5e6bd24157055345b7c6f94e402768ae98b0b866
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-06-22 20:09:50 +0300

    better error handling

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, jsonify
 
 from models import db
 import settings
 import views
+
+
+def error_handler(error):
+    return jsonify(dict(status='error', error=error.description)), error.code
 
 
 def create_app():
@@ -13,6 +17,8 @@ def create_app():
 
     app.add_url_rule('/ping', view_func=views.ping)
     app.add_url_rule('/user', view_func=views.UserView.as_view('user'))
+
+    app.register_error_handler(400, error_handler)
 
     return app
 
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import (
+    abort,
     jsonify,
     request,
 )
@@ -19,13 +20,13 @@ def ping():
 class UserView(MethodView):
     def post(self):
         if 'username' not in request.form:
-            return jsonify(dict(status='error', error='no username')), 400
+            return abort(400, 'no username')
 
         username = request.form['username']
         try:
             db.session.add(User(name=username))
             db.session.commit()
         except IntegrityError:
-            return jsonify(dict(status='error', error='user already exists')), 400
+            return abort(400, 'user already exists')
 
         return jsonify(dict(status='ok'))
