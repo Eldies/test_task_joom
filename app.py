3f137c65a17261ad946ec215ba37e3c129681450
Dmitry Lavrukhin <dlavrukhin@yandex.ru>
2022-06-22 20:10:49 +0300

    try to recreate db on app start

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     db.init_app(app)
 
+    # TODO proper migrations in case of actually using this app
+    with app.app_context():
+        db.create_all()
+
     app.add_url_rule('/ping', view_func=views.ping)
     app.add_url_rule('/user', view_func=views.UserView.as_view('user'))
 
